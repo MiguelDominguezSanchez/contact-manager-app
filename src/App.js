3343f3ba3b69bfa510bf02059fe167f8b58c8ba2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-27 10:43:50 +0100

    3 – 5. Functional Components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 class App extends Component {
 	render() {
-		const name = 'Larry';
-		const showHello = false;
-		const showMath = true;
-		const num1 = 40;
-		const num2 = 23;
-
-		let math;
-		if (showMath) {
-			math = (
-				<h4>
-					{num1} + {num2} = {num1 + num2}
-				</h4>
-			);
-		} else {
-			math = null;
-		}
-
 		return (
 			<div className="App">
-				<h1>The App Component</h1>
-				{showHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
-				{math}
+				<Header />
+				<Contact />
 			</div>
 		);
 	}
