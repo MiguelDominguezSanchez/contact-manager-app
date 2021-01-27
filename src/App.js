99320c3770efad74e6ae2342e3646a7a7a46a849
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-27 10:59:12 +0100

    3 – 6. Working With Props

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Header />
-				<Contact />
+				<Header branding="Contact Manager 1" />
+				<Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-555" />
+				<Contact
+					name="Karen Smith"
+					email="karen@gmail.com"
+					phone="333-333-333"
+				/>
 			</div>
 		);
 	}
