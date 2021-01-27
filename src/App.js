9b607cfb0077700573ccaab8e9cc9222cb47a8d5
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-27 12:17:42 +0100

    3 – 9. Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Header />
-				<Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-555" />
-				<Contact
-					name="Karen Smith"
-					email="karen@gmail.com"
-					phone="333-333-333"
-				/>
+				<Header branding="Contact Manager" />
+				<div className="container">
+					<Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-555" />
+					<Contact
+						name="Karen Smith"
+						email="karen@gmail.com"
+						phone="333-333-333"
+					/>
+				</div>
 			</div>
 		);
 	}
