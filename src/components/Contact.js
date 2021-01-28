ba019fd2829046b425ec98449d57a01975715224
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-28 08:42:21 +0100

    4 – 4. Changing State From Another Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,10 @@ class Contact extends Component {
 		showContactInfo: false,
 	};
 
+	onDeleteClick = () => {
+		this.props.deleteClickHandler();
+	};
+
 	render() {
 		const { name, email, phone } = this.props.contact;
 		const { showContactInfo } = this.state;
@@ -20,6 +24,12 @@ class Contact extends Component {
 							this.setState({ showContactInfo: !this.state.showContactInfo })
 						}
 						className="fas fa-sort-down"
+						style={{ cursor: 'pointer' }}
+					/>
+					<i
+						className="fas fa-times"
+						style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+						onClick={this.onDeleteClick}
 					></i>
 				</h4>
 				{showContactInfo ? (
@@ -35,6 +45,7 @@ class Contact extends Component {
 
 Contact.propTypes = {
 	contact: PropTypes.object.isRequired,
+	deleteClickHandler: PropTypes.func.isRequired,
 };
 
 export default Contact;
